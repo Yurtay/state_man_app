c6ec282729c7fe18626f67be8f3dbc5141653c09
Igor <yurtay2017@yandex.ru>
2024-02-27 17:34:32 +0500

    create page

--- src/redux/reduxMainPage.jsx
+++ src/redux/reduxMainPage.jsx
@@ -1,9 +1,13 @@
 const ReduxMainPage = () => {
+  console.log(JSON.stringify([1, 2, 5, 7]));
   return (
     <>
       <div className="div_container">
         <h1>ReduxMainPage</h1>
       </div>
+      <pre>
+        <code>&lt;div&gt;Какой-то текст&lt;/div&gt;</code>
+      </pre>
     </>
   );
 };
